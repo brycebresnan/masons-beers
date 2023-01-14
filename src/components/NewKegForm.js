434c500385c14a9297b1c4b26670c42de3c8fefa
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-14 00:33:56 -0800

    create form for adding new keg

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,31 @@
+import { v4 } from 'uuid';
+import PropTypes from "prop-types"; 
+import ReusableForm from "./ReusableForm";
+
+function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value, 
+      brewery: event.target.location.value, 
+      style: event.target.issue.value,
+      abv: event.target.abv.value,
+      id: v4()
+    });
+  }
+
+  return (
+    <>
+      <ReusableForm 
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Create Keg" />
+    </>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
