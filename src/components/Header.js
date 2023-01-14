d3a2f9196a22c1637f8ef7e337f64adb22f1a0c9
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-13 23:52:03 -0800

    Add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
+import { Heading, Hero } from 'react-bulma-components';
 
 function Header() {
   return (
     <>
-      <h1>Mason's Beers</h1>
+      <Hero hasNavbar={true}>
+        <Hero.Header>
+          <Heading size={3}>
+            <p>Mason's Beers</p>
+          </Heading>
+        </Hero.Header>
+      </Hero>
     </>
   );
 }
