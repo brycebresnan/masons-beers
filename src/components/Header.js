a9a062b976d71c6dc9b6ed6278c7f45c9b6e5572
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-14 01:20:29 -0800

    remove nav bar causing issues with formating

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Heading, Hero } from 'react-bulma-components';
 function Header() {
   return (
     <>
-      <Hero hasNavbar={true}>
+      <Hero>
         <Hero.Header>
           <Heading size={3}>
             <p>Mason's Beers</p>
