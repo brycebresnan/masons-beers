aedb1aa18202e249b5052dbb8c705d2b12f17c68
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-14 04:26:57 -0800

    add conditional elements and button

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,16 +2,21 @@ import PropTypes from "prop-types";
 
 function Keg(props){
 
+  // const buttonFunc = () => 
+
   return (
     <>
       <div onClick = {() => props.whenKegClicked(props.id)}>
         <h3>{props.brewery} - {props.name}</h3>
         <h4>Style: {props.style}</h4>
         <h4>ABV: {props.abv}%</h4>
-        <p>Quantity: {props.quantity}</p>
-        <button onClick={onBuying}></button>
-        <hr/>
+        {props.quantity == 0 
+          ? <p>Out Of Stock</p>
+          : <p>{props.quantity}</p>
+        }
       </div>
+      <button onClick={() => props.whenBuyingPint(props.id)}> Buy Pint </button>
+      <hr/>
     </>
   );
 }
@@ -23,7 +28,8 @@ Keg.propTypes = {
   abv: PropTypes.string,
   quantity: PropTypes.number,
   id: PropTypes.string,
-  whenKegClicked: PropTypes.func
+  whenKegClicked: PropTypes.func,
+  whenBuyingPint: PropTypes.func
 }
 
 export default Keg;
