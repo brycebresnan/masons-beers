3f0fdde2981147d75cf0b49dbc9315d6b39086d3
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-14 00:20:28 -0800

    create StoreController component

---
+++ src/components/StoreController.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class StoreController extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <>
+      </>
+    );
+  }
+}
+
+export default StoreController;
