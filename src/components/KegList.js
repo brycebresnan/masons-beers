428142b4dacdd8b3651b486d85adac30df2c6340
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-01-14 00:37:22 -0800

    add keg props

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -4,10 +4,25 @@ import PropTypes from "prop-types";
 function KegList(props){
 
   return (
-    <>      
+    <>
+      <hr/>
+      {props.kegList.map((keg) =>
+        <Keg 
+          whenTicketClicked={props.onKegSelection}
+          name={keg.name}
+          brewery={keg.brewery}
+          style={keg.style}
+          abv={keg.abv}
+          id={keg.id}
+          key={keg.id}/>
+      )}
     </>
   );
 }
 
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+};
 
 export default KegList;
